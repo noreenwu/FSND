c569360ea86c881b386eb0cd5687100870a37a85
Noreen Wu <noreenwu@gmail.com>
2019-11-11 17:22:52 -0500

    feat: finished adding artist records

--- projects/01_fyyur/starter_code/load_artists.py
+++ projects/01_fyyur/starter_code/load_artists.py
@@ -14,15 +14,36 @@ artist1 = Artist(artist_id=4,
                  image_link=https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
                  past_shows_count=1,
                  upcoming_shows_count=0,
-
-
 )
-# genre2 = Genre(genre_id=2, genre_name='Reggae')
-# genre3 = Genre(genre_id=3, genre_name='Swing')
-# genre4 = Genre(genre_id=4, genre_name='Classical')
-# genre5 = Genre(genre_id=5, genre_name='Folk')
+
+artist2 = Artist(
+    artist_id=5,
+    name="Matt Quevedo",
+    # genres=["Jazz"],
+    city="New York",
+    state=NY",
+    phone="300-400-5000",
+    facebook_link="https://www.facebook.com/mattquevedo923251523",
+    seeking_venue=False,
+    image_link="https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    past_shows_count=1,
+    upcoming_shows_count=0
+)
+
+artist3=Artist(
+    artist_id=6,
+    artist_id="The Wild Sax Band",
+#    "genres": ["Jazz", "Classical"],
+    city="San Francisco",
+    state="CA",
+    phone="432-325-5432",
+    seeking_venue=False,
+    image_link="https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+    past_shows_count=0,
+    upcoming_shows_count=3
+)
 
 db.session.add(artist1)
+db.session.add(artist2)
+db.session.ad(artist3)
 db.session.commit()
-
-
